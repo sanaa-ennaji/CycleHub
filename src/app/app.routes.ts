9f969261a4131dbf771f00acbb9d95180ad7f231
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-04 15:00:24 +0100

    Register the Reducer in the Store

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/Auth/register/register.component';
 
 export const routes: Routes = [
-    {}
+    {path: '' , component: RegisterComponent}
 ];
