6e92532f2e32f87bf2c49cc5513c7f60d7034689
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-04 14:07:51 +0100

    Create a User Model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+
+export interface User {
+    id?: string ;
+    email:string;
+    paeeword: string;
+    firstName: string;
+    address: string;
+    phoneNumber:string;
+    dateOfBirth: Date;
+    profilePicture?: string;
+    
+}
