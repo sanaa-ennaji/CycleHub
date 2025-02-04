d18418a3963b68c48e1e0b62904422ddd944570f
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-04 14:35:48 +0100

    Reducer

--- src/app/store/auth/auth.actions.ts
+++ src/app/store/auth/auth.actions.ts
@@ -1,17 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 import { User } from '../../models/user.model';
 
-export const register = createAction(
-    '[Auth] Register',
-    props <{user: User}>()
-);
-
-export const registerSuccess = createAction(
-    '[Auth] Register Success',
+export const registerUser = createAction(
+    '[Auth] Register User',
     props<{ user: User }>()
-  );
-  
-  export const registerFailure = createAction(
-    '[Auth] Register Failure',
-    props<{ error: string }>()
   );
---
+++ src/app/store/auth/auth.reducer.ts
@@ -0,0 +1,19 @@
+import { createReducer, on } from '@ngrx/store';
+import { registerUser } from './auth.actions';
+import { User } from '../../models/user.model';
+
+export interface AuthState {
+    user: User | null;
+  }
+  
+  const initialState: AuthState = {
+    user: JSON.parse(localStorage.getItem('user') || 'null'),
+  };
+  
+  export const authReducer = createReducer(
+    initialState,
+    on(registerUser, (state, { user }) => {
+      localStorage.setItem('user', JSON.stringify(user));
+      return { ...state, user };
+    })
+  );
