88082be9654e8f54e63998adc30f915148350d32
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-05 09:13:14 +0100

    trying to resolve the tailwind prolem

--- src/app/components/Auth/register/register.component.ts
+++ src/app/components/Auth/register/register.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { registerUser } from '../../../store/auth/auth.actions';
+import { User } from '../../../models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'],
+  imports: [ReactiveFormsModule]
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private store: Store,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      address: ['', Validators.required],
+      phone: ['', Validators.required],
+      birthDate: ['', Validators.required],
+    });
+  }
+
+  onRegister() {
+    if (this.registerForm.valid) {
+      const user: User = this.registerForm.value;
+      this.store.dispatch(registerUser({ user }));
+
+      alert('Registration Successful! Redirecting to Dashboard...');
+      this.router.navigate(['/dashboard']);
+    }
+  }
 }
