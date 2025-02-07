b9ec32af7015c5fb971aefaa2cdb58aa90c36b52
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-07 11:20:51 +0100

    import navbar in login

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { User } from '../../../models/user.model';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../../Shared/navbar/navbar.component';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ CommonModule , NavbarComponent],
+  standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
+ 
+
 })
 export class LoginComponent {
 
+  
 }
