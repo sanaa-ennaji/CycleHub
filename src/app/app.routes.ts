6d6d0cb80e6cccb315993782380c95db3d865e49
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-07 11:30:55 +0100

    add login path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/user/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/user/login/login.component';
 
 export const routes: Routes = [
     {path: '' , component: HomeComponent},
-    {path: 'register', component: RegisterComponent}
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent}
 ];
