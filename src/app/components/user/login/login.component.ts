cb256ae304194bf3d840c8d8a6e34f065b7fa63c
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-07 14:51:58 +0100

    Dispatch Login and Logout Actions in the Component

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { User } from '../../../models/user.model';
 import { Router } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { loginUser, logoutUser } from '../../../store/auth/auth.actions';
+import { AuthState } from '../../../store/auth/auth.reducer';
 import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
 import { NavbarComponent } from '../../../Shared/navbar/navbar.component';
 
 
@@ -17,8 +18,20 @@ import { NavbarComponent } from '../../../Shared/navbar/navbar.component';
 })
 export class LoginComponent {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private store: Store<{ auth: AuthState }> ) {}
   navigateToRegister() {
     this.router.navigate(['/register']);
+  }
+
+  email = '';
+  password = '';
+
+
+  login() {
+    this.store.dispatch(loginUser({ email: this.email, password: this.password }));
+  }
+
+  logout() {
+    this.store.dispatch(logoutUser());
   }
 }
