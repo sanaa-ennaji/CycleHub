03ead0cd5ae30620e6a5de49ee27bbc6564cba70
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-07 09:16:14 +0100

    import routes

--- src/app/Shared/navbar/navbar.component.ts
+++ src/app/Shared/navbar/navbar.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
