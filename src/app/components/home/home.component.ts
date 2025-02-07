d3ccb7c6d4c8840382aa44d9f26cdc66e82ca469
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-07 11:03:05 +0100

    import navbar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { NavbarComponent } from '../../Shared/navbar/navbar.component';
 @Component({
   selector: 'app-home',
@@ -8,10 +7,7 @@ import { NavbarComponent } from '../../Shared/navbar/navbar.component';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
-  navigateToRegister() {
-    this.router.navigate(['/register']);
-  }
+
   
 }
 
