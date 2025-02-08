7fe6be7ccb70758b83517ee99019ab93f84fefaa
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-08 17:00:16 +0100

    add style to the form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/user/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/user/login/login.component';
+import { DemandeRequestComponent } from './components/Dashboard/demande-request/demande-request.component';
 
 export const routes: Routes = [
     {path: '' , component: HomeComponent},
     {path: 'register', component: RegisterComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'demande', component: DemandeRequestComponent},
+    {path: 'login', component: LoginComponent},
+
 ];
