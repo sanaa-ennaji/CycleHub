6f269833ac447259b83b7adb8fc6f10f53cb5443
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 00:24:29 +0100

    made demnde list

--- src/app/components/Dashboard/demande-list/demande-list.component.ts
+++ src/app/components/Dashboard/demande-list/demande-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { DemandeRequestComponent } from '../demande-request/demande-request.component';
 @Component({
   selector: 'app-demande-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, DemandeRequestComponent],
   templateUrl: './demande-list.component.html',
   styleUrl: './demande-list.component.css'
 })
