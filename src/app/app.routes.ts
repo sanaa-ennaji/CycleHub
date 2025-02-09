20ce60f422ee277f03dcf47340fbee017e7c3a68
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-09 18:49:22 +0100

    Auto-commit on 09/02/2025 18:49:22,29

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RegisterComponent } from './components/user/register/register.component
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/user/login/login.component';
 import { DemandeRequestComponent } from './components/Dashboard/demande-request/demande-request.component';
+import { SidebareComponent } from './components/Shared/sidebare/sidebare.component';
 
 export const routes: Routes = [
     {path: '' , component: HomeComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'demande', component: DemandeRequestComponent},
     {path: 'login', component: LoginComponent},
+    {path: 'side', component : SidebareComponent}
 
 ];
