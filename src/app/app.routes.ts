cf9692067a5bc0cc78701e54b31221b23818fe04
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 00:50:42 +0100

    add sidebare

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,15 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/user/login/login.component';
 import { SidebareComponent } from './components/Shared/sidebare/sidebare.component';
 import { DemandeListComponent } from './components/Dashboard/demande-list/demande-list.component';
+import { CollectionsListComponent } from './components/Collectors/collections-list/collections-list.component';
+
 
 export const routes: Routes = [
     {path: '' , component: HomeComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'demande', component: DemandeListComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'side', component : SidebareComponent}
+    {path: 'side', component : SidebareComponent},
+    {path: 'collectionsList', component: CollectionsListComponent}
 
 ];
--- src/app/components/Dashboard/demande-list/demande-list.component.ts
+++ src/app/components/Dashboard/demande-list/demande-list.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemandeRequestComponent } from '../demande-request/demande-request.component';
-import { SidebareComponent } from "../../Shared/sidebare/sidebare.component";
+
 @Component({
   selector: 'app-demande-list',
   standalone: true,
-  imports: [CommonModule, DemandeRequestComponent, SidebareComponent],
+  imports: [CommonModule, DemandeRequestComponent],
   templateUrl: './demande-list.component.html',
   styleUrl: './demande-list.component.css'
 })
