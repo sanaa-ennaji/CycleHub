9cac44568b63f2ae191070a95b6ac18995e4ef39
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 09:46:39 +0100

    update on   9:46:39,28

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { User } from '../../../models/user.model';
 import { AuthState } from '../../../store/auth/auth.state';
 import { updateUser } from '../../../store/auth/auth.actions';
-import { selectCurrentUser} from '../../../store/auth/auth.selectors';
-import { CommonModule } from '@angular/common';
+import { selectCurrentUser } from '../../../store/auth/auth.selectors';
+
 @Component({
   selector: 'app-profile',
-  imports: [ReactiveFormsModule,  CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent  implements OnInit{
-
+export class ProfileComponent implements OnInit {
   profileForm: FormGroup;
   currentUser: User | null = null;
+  selectedFile: File | null = null;
+  previewImage: string | null = null;
 
   constructor(private fb: FormBuilder, private store: Store<AuthState>) {
     this.profileForm = this.fb.group({
@@ -38,10 +38,24 @@ export class ProfileComponent  implements OnInit{
           address: user.address,
           phoneNumber: user.phoneNumber,
           dateOfBirth: user.dateOfBirth ? new Date(user.dateOfBirth).toISOString().split('T')[0] : '',
-          profilePicture: user.profilePicture || '',
         });
+
+        this.previewImage = user.profilePicture || null;
       }
     });
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.previewImage = e.target.result;
+      };
+      reader.readAsDataURL(this.selectedFile);
+    }
   }
 
   onSubmit(): void {
@@ -50,7 +64,9 @@ export class ProfileComponent  implements OnInit{
         ...this.currentUser,
         ...this.profileForm.value,
         dateOfBirth: new Date(this.profileForm.value.dateOfBirth),
+        profilePicture: this.previewImage || this.currentUser.profilePicture, 
       };
+
       this.store.dispatch(updateUser({ user: updatedUser }));
     }
   }
