c1ed1b3e37d01d6fe7314ab5a907c76a776db0d9
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 10:40:03 +0100

    update on  10:40:03,25

--- src/app/store/collection/collection.reducer.ts
+++ src/app/store/collection/collection.reducer.ts
@@ -13,8 +13,7 @@ export const initialState: CollectionState = {
 export const collectionReducer = createReducer(
   initialState,
   on(addCollection, (state, { collection }) => {
-    const updatedCollections = [...state.collections, collection];
-    // Optionally store in localStorage from the reducer itself (but not ideal)
+    const updatedCollections = [...state.requests, collection];
     localStorage.setItem('collections', JSON.stringify(updatedCollections));
     return { ...state, collections: updatedCollections };
   }),
