c90f58fa220555486629450c911889de13fe636c
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 01:14:45 +0100

    requests

--- src/app/components/Collectors/request-list/request-list.component.ts
+++ src/app/components/Collectors/request-list/request-list.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { SidebareComponent } from "../../Shared/sidebare/sidebare.component";
 @Component({
   selector: 'app-request-list',
-  imports: [],
+  imports: [SidebareComponent],
   templateUrl: './request-list.component.html',
   styleUrl: './request-list.component.css'
 })
