f2f2b3e3c6e342849d69bca3da392a48298e6392
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 21:45:51 +0100

    autorisations done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,19 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/user/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/user/login/login.component';
-import { SidebareComponent } from './components/Shared/sidebare/sidebare.component';
 import { DemandeListComponent } from './components/Dashboard/demande-list/demande-list.component';
 import { CollectionsListComponent } from './components/Collectors/collections-list/collections-list.component';
 import { ProfileComponent } from './components/user/profile/profile.component';
 import { RequestListComponent } from './components/Collectors/request-list/request-list.component';
-
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    {path: '' , component: HomeComponent},
-    {path: 'register', component: RegisterComponent},
-    {path: 'demande', component: DemandeListComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'side', component : SidebareComponent},
-    {path: 'collectionsList', component: CollectionsListComponent},
-    {path: 'requestList', component: RequestListComponent },
-    {path: 'profile', component : ProfileComponent}
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
 
+  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] }, 
+  { path: 'demande', component: DemandeListComponent, canActivate: [authGuard] },
+  { path: 'collectionsList', component: CollectionsListComponent, canActivate: [authGuard] }, 
+  { path: 'requestList', component: RequestListComponent, canActivate: [authGuard] } 
 ];
