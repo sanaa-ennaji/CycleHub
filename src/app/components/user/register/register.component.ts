3ed989bf92ee4de408c64ea5129d1d593ba18e5d
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 12:05:06 +0100

    update on  12:05:06,26

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -1,51 +1,71 @@
+// src/app/components/demande-request/demande-request.component.ts
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { registerUser } from '../../../store/auth/auth.actions';
-import { User } from '../../../models/user.model';
-import { Router } from '@angular/router';
+import { addCollection } from '../../../store/collection/collection.actions';
+import { Collection } from '../../../models/Collection.model';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from '../../Shared/navbar/navbar.component';
-
+import { Status } from '../../../models/Status.enum';
+import { StoreModule, provideStore } from '@ngrx/store';
+import { EffectsModule, provideEffects } from '@ngrx/effects';
+import { collectionReducer } from '../../../store/collection/collection.reducer';
+import { CollectionEffects } from '../../../store/collection/collection.effects';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
+  selector: 'app-demande-request',
   standalone: true,
-  styleUrls: ['./register.component.css'],
-  imports: [ReactiveFormsModule, CommonModule , NavbarComponent],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    provideStore({ collections: collectionReducer }),
+    provideEffects(CollectionEffects),
+  ],
+  templateUrl: './demande-request.component.html',
+  styleUrls: ['./demande-request.component.css'],
 })
-export class RegisterComponent {
-  registerForm: FormGroup;
+export class DemandeRequestComponent {
+  requestForm: FormGroup;
+  isOpen: boolean = false;
 
-  constructor(
-    private fb: FormBuilder,
-    private store: Store,
-    private router: Router
-    
-  )
-   {
-    this.registerForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+  constructor(private fb: FormBuilder, private store: Store) {
+    this.requestForm = this.fb.group({
+      wasteType: ['', Validators.required],
+      estimatedWeight: ['', [Validators.required, Validators.min(1000)]],
       address: ['', Validators.required],
-      phone: ['', Validators.required],
-      birthDate: ['', Validators.required],
+      date: ['', Validators.required],
+      timeSlot: ['', Validators.required],
+      additionalNotes: [''],
     });
-
   }
 
-  onRegister() {
-    if (this.registerForm.valid) {
-      const user: User = this.registerForm.value;
-      this.store.dispatch(registerUser({ user }));
-      this.router.navigate(['/']);
+  openModal() {
+    this.isOpen = true;
+  }
+
+  closeModal() {
+    this.isOpen = false;
+    this.resetForm();
+  }
+
+  onSubmit() {
+    if (this.requestForm.valid) {
+      const request: Collection = {
+        ...this.requestForm.value,
+        id: this.generateId(),
+        status: Status.PENDING,
+      };
+      console.log('Dispatching addCollection action:', request);
+      this.store.dispatch(addCollection({ collection: request }));
+    } else {
+      console.error('Form is invalid');
     }
   }
-  navigateToLogin() {
-    this.router.navigate(['/login']);
+
+  private generateId(): string {
+    return Math.random().toString(36).substr(2, 9);
   }
 
+  private resetForm() {
+    this.requestForm.reset();
+  }
 }
