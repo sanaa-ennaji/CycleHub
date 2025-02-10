a0346a22491e2745ed66f761e11f4f073230d8a9
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 20:14:12 +0100

    points for the user

--- src/app/components/Collectors/collections-list/collections-list.component.ts
+++ src/app/components/Collectors/collections-list/collections-list.component.ts
@@ -16,6 +16,7 @@ export class CollectionsListComponent implements OnInit {
   collections: Collection[] = [];
   selectedCollection: Collection | null = null; 
   statusOptions = Object.values(Status); 
+  userPoints: number = 0;
 
   ngOnInit() {
     this.loadCollectionsFromLocalStorage();
@@ -40,18 +41,45 @@ export class CollectionsListComponent implements OnInit {
     this.selectedCollection = null;
   }
 
-  updateCollectionStatus() {
+  private loadUserPoints() {
+    const storedPoints = localStorage.getItem('userPoints');
+    this.userPoints = storedPoints ? parseInt(storedPoints, 10) : 0;
+  }
+
+  private saveUserPoints() {
+    localStorage.setItem('userPoints', this.userPoints.toString());
+  }
+
+  updateStatus() {
     if (this.selectedCollection) {
       const index = this.collections.findIndex(c => c.id === this.selectedCollection?.id);
       if (index !== -1) {
         this.collections[index].status = this.selectedCollection.status;
-        this.updateLocalStorage();
+
+       
+        if (this.selectedCollection.status === Status.CONFIRMED && !this.collections[index].validated) {
+          const points = this.calculatePoints(this.selectedCollection);
+          this.userPoints += points;
+          this.collections[index].validated = true; 
+          this.saveUserPoints();
+        }
+
+        localStorage.setItem('collections', JSON.stringify(this.collections));
       }
+      this.selectedCollection = null;
       this.closeUpdateModal();
     }
   }
 
-  private updateLocalStorage() {
-    localStorage.setItem('collections', JSON.stringify(this.collections));
+
+  calculatePoints(collection: Collection): number {
+    const rate: { [key: string]: number } = {
+      "Plastique": 2,
+      "Verre": 1,
+      "Papier": 1,
+      "Metal": 5
+    };
+    return (rate[collection.wasteType] || 0) * collection.estimatedWeight;
   }
 }
+
