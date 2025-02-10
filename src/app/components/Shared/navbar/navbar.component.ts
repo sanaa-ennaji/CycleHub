cf3659bf50b8f457bd3c0a29d3ee23d78cb8b571
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-11 00:05:34 +0100

    add images

--- src/app/components/Shared/navbar/navbar.component.ts
+++ src/app/components/Shared/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { logoutUser } from '../../../store/auth/auth.actions';
 import { AuthState } from '../../../store/auth/auth.state';
@@ -9,7 +9,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports : [CommonModule],
+  imports : [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
