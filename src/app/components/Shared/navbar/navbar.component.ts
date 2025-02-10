159446d9a5945e5e1b9a79cc0b63e2b07bea81bb
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 22:58:00 +0100

    add logout button

--- src/app/components/Shared/navbar/navbar.component.ts
+++ src/app/components/Shared/navbar/navbar.component.ts
@@ -1,19 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { logoutUser } from '../../../store/auth/auth.actions';
+import { AuthState } from '../../../store/auth/auth.state';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
   standalone: true,
+  imports : [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
   imagePath = 'assets/logo.png';
+  currentUser$: Observable<any>; 
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private store: Store<{ auth: AuthState }>) {
+    this.currentUser$ = this.store.select(state => state.auth.currentUser);
+  }
+
   navigateToRegister() {
     this.router.navigate(['/register']);
   }
+
+  logout() {
+    this.store.dispatch(logoutUser());
+    this.router.navigate(['/login']); 
+  }
 }
+
