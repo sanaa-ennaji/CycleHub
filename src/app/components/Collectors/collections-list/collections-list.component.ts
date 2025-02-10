f52486dc36b3e412215d881ff98ea8b7623a491a
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 01:16:50 +0100

    add collections for update

--- src/app/components/Collectors/collections-list/collections-list.component.ts
+++ src/app/components/Collectors/collections-list/collections-list.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { SidebareComponent } from "../../Shared/sidebare/sidebare.component";
 
 @Component({
   selector: 'app-collections-list',
- 
+  imports: [SidebareComponent],
   templateUrl: './collections-list.component.html',
   styleUrl: './collections-list.component.css'
 })
