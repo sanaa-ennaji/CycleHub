3cf52cebb89a9330e7ed7928ea151e4b1ce7bd6d
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 01:11:55 +0100

    styling the table done

--- src/app/components/Dashboard/demande-list/demande-list.component.ts
+++ src/app/components/Dashboard/demande-list/demande-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemandeRequestComponent } from '../demande-request/demande-request.component';
+import { NavbarComponent } from '../../Shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-demande-list',
   standalone: true,
-  imports: [CommonModule, DemandeRequestComponent],
+  imports: [CommonModule, DemandeRequestComponent, NavbarComponent],
   templateUrl: './demande-list.component.html',
   styleUrl: './demande-list.component.css'
 })
