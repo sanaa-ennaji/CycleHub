81c88119996e1bfbdddee1a772c70ea84a0e363d
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 11:26:05 +0100

    update on  11:26:05,78

--- src/app/store/collection/collection.effects.ts
+++ src/app/store/collection/collection.effects.ts
@@ -5,6 +5,7 @@ import { addCollection, updateCollection, deleteCollection } from './collection.
 import { tap } from 'rxjs/operators';
 import { createEffect } from '@ngrx/effects'; 
 import { selectCollections } from './collection.selector';
+
 @Injectable()
 export class CollectionEffects {
   constructor(private actions$: Actions, private store: Store) {}
@@ -13,11 +14,12 @@ export class CollectionEffects {
     () =>
       this.actions$.pipe(
         ofType(addCollection, updateCollection, deleteCollection),
-        tap(() => {
-            console.log('addCollection action received:');
+        tap((action) => {
+          console.log('addCollection action received:', action);  
           this.store
             .select(selectCollections)  
             .subscribe((requests) => {
+              console.log('Saving collections to localStorage:', requests); 
               localStorage.setItem('collections', JSON.stringify(requests));
             });
         })
