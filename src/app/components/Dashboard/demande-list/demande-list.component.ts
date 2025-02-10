1980dbd984ffed9e1d4ac02478a43752c9b43047
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-10 14:05:35 +0100

    demande-list.component.ts

--- src/app/components/Dashboard/demande-list/demande-list.component.ts
+++ src/app/components/Dashboard/demande-list/demande-list.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemandeRequestComponent } from '../demande-request/demande-request.component';
 import { NavbarComponent } from '../../Shared/navbar/navbar.component';
-
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { CollectionState } from '../../../store/collection/collection.reducer';
+import { Collection } from '../../../models/Collection.model';
 @Component({
   selector: 'app-demande-list',
   standalone: true,
@@ -11,5 +14,9 @@ import { NavbarComponent } from '../../Shared/navbar/navbar.component';
   styleUrl: './demande-list.component.css'
 })
 export class DemandeListComponent {
+  collections$: Observable<Collection[]>;
 
+  constructor(private store: Store<{ collections: CollectionState }>) {
+    this.collections$ = this.store.pipe(select(state => state.collections.requests));
+  }
 }
